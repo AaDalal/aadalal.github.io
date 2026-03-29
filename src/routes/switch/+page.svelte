b8d4fe8805642255533b7b516b120a364756d43f
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2026-03-28 17:27:06 -0700

    Add /switch (a static page for testing)

---
+++ src/routes/switch/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const isOn = true;
+</script>
+
+<svelte:head>
+	<title>switch | aagam's blog</title>
+</svelte:head>
+
+<main class="flex items-center align-center">
+	<p class="text-2xl">
+	the switch is <span class="font-bold italic">{isOn ? 'alive' : 'dead'}</span>
+	</p>
+</main>
