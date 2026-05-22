3de9cea2179c2628ffa5e7c17cc386f5257e1260
Aagam Dalal <57609353+AaDalal@users.noreply.github.com>
2026-05-21 21:18:54 -0700

    Remove switch

--- src/routes/switch/+page.svelte
+++
@@ -1,9 +0,0 @@
-<svelte:head>
-	<title>switch | aagam's blog</title>
-</svelte:head>
-
-<main class="flex items-center align-center">
-	<p class="text-2xl">
-	the switch is <span class="font-bold italic">on</span>
-	</p>
-</main>
